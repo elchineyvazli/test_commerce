5973d7c0ebaa4041a81258b59ac2e9ae2c1f53e2
elchineyvazli <elchineyvazli22@gmail.com>
2025-01-21 08:51:54 +0400

    commit

--- navbar/main.js
+++ navbar/main.js
@@ -1,14 +1,14 @@
 const tabs = document.getElementsByClassName("tab");
 tabs_arr = Array.from(tabs)
 
-product_url = "http://127.0.0.1:8000/product/"
-tabs_arr.forEach(async el => {
+products_url = "http://127.0.0.1:8000/api/categories/"
+tabs_arr.forEach(async (el, ind) => {
     // try {
-    const response = await fetch(product_url);
+    const response = await fetch(products_url);
     const data = await response.json();
-    console.log(data);
-    // el.innerText = "e";
-    
+    console.log(data[0].name);
+    el.innerText = data[ind].name;
+
     // } catch (error) {
     //     console.error("Hata oluştu:", error);
     // }
